310bb3b71cd13025c1bb52e2cc3519866a37bd24
Hamz-1 <156629027+Hamz-1@users.noreply.github.com>
2024-01-20 12:27:11 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+alert('Greetings! Welcome Hope you Enjoy you stay')
+
+var age = prompt('Enter your age: ')
+
+console.log(age);
+
+if (age < 25) {
+    console.log('You are a student');
+    alert('You are a student')
+}
+else{
+    console.log('You are not a student');
+    alert('You are not a student')
+}
